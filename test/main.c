ea5af5e3338bc05082e81ff7792561c3463f5e3f
pokr <mistrpokr@outlook.com>
2022-12-12 14:55:40 -0800

    add UART

--- test/main.c
+++ test/main.c
@@ -1,11 +1,22 @@
 #include <inttypes.h>
 #include <stdbool.h>
+#include <stddef.h>
 
 #define BIT(x) (1UL << (x)) // BIT(3) = 0000 1000
 #define PIN(bank, num) ((((bank) - 'A') << 8) | (num))
 #define PINNO(pin) (pin & 255)
 #define PINBANK(pin) (pin >> 8)
 
+#define SYS_FREQ 16000000
+
+static inline void
+spin(volatile uint32_t count)
+{
+  while (count--)
+    asm("nop");
+}
+
+/* RCC */
 struct rcc
 {
   volatile uint32_t CR, PLLCFGR, CFGR, CIR, AHB1RSTR, AHB2RSTR, AHB3RSTR,
@@ -16,21 +27,7 @@ struct rcc
 };
 #define RCC ((struct rcc*)0x40023800)
 
-struct gpio
-{
-  volatile uint32_t MODER, OTYPER, OSPEEDR, PUPDR, IDR, ODR, BSRR, LCKR, AFR[2];
-};
-
-#define GPIO(bank) ((struct gpio*)(0x40020000 + bank * 0x400))
-
-enum
-{
-  GPIO_MODE_INPUT,
-  GPIO_MODE_OUTPUT,
-  GPIO_MODE_AF,
-  GPIO_MODE_ANALOG
-};
-
+/* SysTick */
 struct systick
 {
   volatile uint32_t CTRL, LOAD, VAL, CALIB;
@@ -69,12 +66,29 @@ timer_expired(uint32_t* t, uint32_t period, uint32_t now)
   return true;
 }
 
+/* GPIO */
+struct gpio
+{
+  volatile uint32_t MODER, OTYPER, OSPEEDR, PUPDR, IDR, ODR, BSRR, LCKR, AFR[2];
+};
+
+#define GPIO(bank) ((struct gpio*)(0x40020000 + bank * 0x400))
+
+enum
+{
+  GPIO_MODE_INPUT,
+  GPIO_MODE_OUTPUT,
+  GPIO_MODE_AF,
+  GPIO_MODE_ANALOG
+};
+
 static inline void
 gpio_set_mode(uint16_t pin, uint8_t mode)
 {
   struct gpio* gpio = GPIO(PINBANK(pin));
   int n = PINNO(pin);
-  gpio->MODER &= ~(3U << (n * 2)); // 3U = 0b11
+  RCC->AHB1ENR |= BIT(PINBANK(pin)); // Enable GPIO clock in RCC
+  gpio->MODER &= ~(3U << (n * 2));   // 3U = 0b11
   gpio->MODER |= (mode & 3U) << (n * 2);
 }
 
@@ -86,31 +100,106 @@ gpio_write(uint16_t pin, bool val)
   // True: offset=0, write to BSy; False: offset=16, write to BRy
 }
 
+/* Set alternate function for GPIO */
 static inline void
-spin(volatile uint32_t count)
+gpio_set_af(uint16_t pin, uint8_t af_num)
 {
-  while (count--)
-    asm("nop");
+  struct gpio* gpio = GPIO(PINBANK(pin));
+  int n = PINNO(pin);
+  bool afr_h_l = (n >> 3) & 0x1;
+  gpio->AFR[afr_h_l] &=
+    ~(15UL << ((n & 7) * 4)); // Reset specified AFRLx 4 bits
+  gpio->AFR[afr_h_l] |= (af_num) << ((n & 7) * 4);
+  // gpio->AFR[afr_h_l] |= ((uint32_t)af_num) << ((n & 7) * 4)
+}
+
+/* UART */
+struct uart
+{
+  volatile uint32_t SR, DR, BRR, CR1, CR2, CR3, GTPR;
+};
+#define UART1 ((struct uart*)0x40011000)
+#define UART2 ((struct uart*)0x40004400)
+#define UART3 ((struct uart*)0x40004800)
+
+static inline void
+uart_init(struct uart* uart, unsigned long baud)
+{
+  uint8_t af = 0;
+  uint16_t rx = 0, tx = 0;
+
+  if (uart == UART1)
+    RCC->APB2ENR |= BIT(4); // Set bit 4 of APB2ENR
+  if (uart == UART2)
+    RCC->APB1ENR |= BIT(17);
+  if (uart == UART3)
+    RCC->APB1ENR |= BIT(18);
+
+  if (uart == UART1)
+    af = 7, tx = PIN('A', 9), rx = PIN('A', 10);
+  // af = 4, tx = PIN('A', 9), rx = PIN('A', 10);
+  if (uart == UART2)
+    af = 4, tx = PIN('A', 2), rx = PIN('A', 3);
+  if (uart == UART3)
+    af = 7, tx = PIN('D', 8), rx = PIN('D', 9);
+
+  gpio_set_mode(tx, GPIO_MODE_AF);
+  gpio_set_af(tx, af);
+  gpio_set_mode(rx, GPIO_MODE_AF);
+  gpio_set_af(rx, af);
+
+  uart->CR1 = 0;
+  uart->BRR = SYS_FREQ / baud;
+  uart->CR1 |= BIT(13) | BIT(2) | BIT(3);
+}
+
+static inline int
+uart_read_ready(struct uart* uart)
+{
+  return uart->SR & BIT(5); // check if RXNE bit is set
+}
+
+static inline uint8_t
+uart_read_byte(struct uart* uart)
+{
+  return (uint8_t)(uart->DR & 255);
+}
+
+static inline void
+uart_write_byte(struct uart* uart, uint8_t byte)
+{
+  uart->DR = byte;
+  while ((uart->SR & BIT(7)) == 0)
+    spin(1);
+}
+
+static inline void
+uart_write_buffer(struct uart* uart, char* buf, size_t len)
+{
+  while (len-- > 0)
+    uart_write_byte(uart, *(uint8_t*)buf++);
 }
 
 int
 main(void)
 {
+  systick_init(SYS_FREQ / 1000);
+  uint32_t timer, period = 250;
+
   uint16_t led_blue = PIN('B', 7);
   uint16_t led_red = PIN('B', 14);
-  RCC->AHB1ENR |= BIT(PINBANK(led_blue));
-  RCC->AHB1ENR |= BIT(PINBANK(led_red));
   gpio_set_mode(led_blue, GPIO_MODE_OUTPUT);
   gpio_set_mode(led_red, GPIO_MODE_OUTPUT);
 
-  systick_init(16000000 / 1000);
-  uint32_t timer, period = 250;
+  uart_init(UART3, 115200);
 
   while (1) {
     if (timer_expired(&timer, period, s_ticks)) {
       static bool on;
       gpio_write(led_blue, on);
       on = !on;
+
+      uart_write_buffer(UART3, "hi\r\n", 4);
     }
 
     // gpio_write(led_blue, true);
